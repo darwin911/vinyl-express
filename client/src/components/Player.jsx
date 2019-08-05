c7c5ec442f99430a249d8333f83a67a8748aa664
Darwin Smith <darwinpsmith@gmail.com>
2019-08-05 16:47:58 -0400

    refactor Player to functional component

--- client/src/components/Player.jsx
+++ client/src/components/Player.jsx
@@ -1,112 +1,90 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Sound from 'react-sound';
 import { Alert } from 'react-bootstrap';
 import TimeFormat from 'hh-mm-ss';
 import vinyl from '../assets/vinyl-red.png';
 
-class Player extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      playbackRate: 1,
-      volume: 85,
-      duration: 0,
-      position: 0
-    };
-  }
+const Player = ({ togglePlay, currentTrack, playStatus, errorMessage }) => {
+  const [playbackRate, setPlaybackRate] = React.useState(1);
+  const [volume, setVolume] = React.useState(90);
+  const [duration, setDuration] = React.useState(0);
+  const [position, setPosition] = React.useState(0);
 
-  handleControlChange = e => {
-    const { name, value } = e.target;
-    this.setState(prevState => ({
-      ...prevState,
-      [name]: parseFloat(value)
-    }));
-  };
-
-  render() {
-    const { currentTrack, playStatus, errorMessage } = this.props;
-    const { playbackRate, volume, position, duration } = this.state;
-    return (
-      <section className='player'>
-        <div className='turntable'>
-          <img
-            className={'vinyl ' + (playStatus === 'PLAYING' ? 'spin' : '')}
-            src={vinyl}
-            alt='vinyl'
-          />
-
-          <div
-            className={
-              'tt-arm ' + (playStatus === 'PLAYING' ? 'tt-play' : 'tt-stop')
-            }
-          />
-
-          <button
-            onClick={() => this.props.togglePlay()}
-            className={
-              'start-stop-btn ' + (playStatus === 'PLAYING' ? 'green' : '')
-            }>
-            &#8227;
-          </button>
-
-          <input
-            className='playback-input'
-            name='playbackRate'
-            type='range'
-            step={0.01}
-            min={0.5}
-            max={3.5}
-            onChange={this.handleControlChange}
-            value={playbackRate}
-          />
-
-          <input
-            className='volume-input'
-            name='volume'
-            type='range'
-            step={1}
-            min={1}
-            max={120}
-            onChange={this.handleControlChange}
-            value={volume}
-          />
-        </div>
-
-        {errorMessage && <Alert variant='dark'>{errorMessage}</Alert>}
-
-        <p className='track-time'>
-          {TimeFormat.fromMs(parseInt(position), 'mm:ss').slice(0, 5)} /{' '}
-          {TimeFormat.fromMs(parseInt(duration), 'mm:ss').slice(0, 5)}
-        </p>
-
-        {currentTrack.title ? (
-          <p
-            style={{
-              fontFamily: 'Helvetica Neue',
-              fontSize: '2rem',
-              lineHeight: '1'
-            }}>
-            {currentTrack.title}
-          </p>
-        ) : (
-          <p>Load a track first!</p>
-        )}
-        <Sound
-          url={currentTrack.url && currentTrack.url}
-          volume={volume}
-          playbackRate={playbackRate}
-          onFinish={() => this.props.togglePlay}
-          onLoad={e => {
-            this.setState({ duration: e.duration });
-          }}
-          onPlaying={e => {
-            this.setState({ position: e.position });
-          }}
-          playStatus={playStatus}
+  return (
+    <section className='player'>
+      <div className='turntable'>
+        <img
+          className={'vinyl ' + (playStatus === 'PLAYING' ? 'spin' : '')}
+          src={vinyl}
+          alt='vinyl'
         />
-      </section>
-    );
-  }
-}
+
+        <div
+          className={
+            'tt-arm ' + (playStatus === 'PLAYING' ? 'tt-play' : 'tt-stop')
+          }
+        />
+
+        <button
+          onClick={() => togglePlay()}
+          className={
+            'start-stop-btn ' + (playStatus === 'PLAYING' ? 'green' : '')
+          }>
+          &#8227;
+        </button>
+
+        <input
+          className='playback-input'
+          name='playbackRate'
+          type='range'
+          step={1}
+          min={0.5}
+          max={2.0}
+          onChange={e => setPlaybackRate(e.target.value)}
+          value={playbackRate}
+        />
+
+        <input
+          className='volume-input'
+          name='volume'
+          type='range'
+          step={1}
+          min={1}
+          max={110}
+          onChange={e => setVolume(parseInt(e.target.value))}
+          value={volume}
+        />
+      </div>
+
+      {errorMessage && <Alert variant='dark'>{errorMessage}</Alert>}
+
+      <p className='track-time'>
+        {TimeFormat.fromMs(parseInt(position), 'mm:ss').slice(0, 5)} /{' '}
+        {TimeFormat.fromMs(parseInt(duration), 'mm:ss').slice(0, 5)}
+      </p>
+
+      {currentTrack.title ? (
+        <p
+          style={{
+            fontFamily: 'Helvetica Neue',
+            fontSize: '2rem',
+            lineHeight: '1'
+          }}>
+          {currentTrack.title}
+        </p>
+      ) : (
+        <p>Load a track first!</p>
+      )}
+      <Sound
+        url={currentTrack.url && currentTrack.url}
+        volume={volume}
+        playbackRate={playbackRate}
+        onLoad={e => setDuration(e.duration)}
+        onPlaying={e => setPosition(e.position)}
+        playStatus={playStatus}
+      />
+    </section>
+  );
+};
 
 export default Player;
